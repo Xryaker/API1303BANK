febee3bc131831b12939137268ee70b0df290e4e
Your Name <you@example.com>
2023-07-17 20:31:05 +0300

    -17 07 2023 API

---
+++ src/main/java/objects/bankGo/Exchange.java
@@ -0,0 +1,13 @@
+package objects.bankGo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Exchange {
+//    @JsonProperty("r030")
+    public int vasiliy;
+
+    public String txt;
+    public double rate;
+    public String cc;
+    public String exchangedate;
+}
